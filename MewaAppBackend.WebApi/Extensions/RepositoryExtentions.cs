fa863497eeeef72bfa3ba18697ac0b6c1ab46e3f
mrdedpl <mrdedpl@wp.pl>
2022-03-31 15:14:50 +0200

    repaired RepositoryExtensions

--- MewaAppBackend.WebApi/Extensions/RepositoryExtentions.cs
+++ MewaAppBackend.WebApi/Extensions/RepositoryExtentions.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace MewaAppBackend.WebApi.Extensions
 {
     public static class RepositoryExtentions
     {
-        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query, System.Linq.Expressions.Expression<Func<T, object>>[] includes1, params Expression<Func<T, object>>[] includes) where T : class
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query, params Expression<Func<T, object>>[] includes) where T : class
         {
             if (includes != null)
                 query = includes.Aggregate(query, (current, include) => current.Include(include));
