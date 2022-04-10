f950cf7ebb52f653ea7c744a7b0d3ece7f47e84f
Wojciech Buńka <wojtek.bunka@gmail.com>
2022-04-10 18:38:47 +0200

    Basic user creation + program.cs clean up

---
+++ MewaAppBackend.WebApi/Configuration/BasicConfig.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MewaAppBackend.WebApi.Configuration
+{
+    public static class BasicConfig
+    {
+        public static void Handle(WebApplicationBuilder builder) 
+        {
+            builder.Services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "MewaAppFront/dist";
+            });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.WithOrigins(new string[]
+                    {
+             "https://localhost:7097",
+             "http://localhost:7097",
+             "http://localhost:5097",
+             "https://localhost:5097",
+             "http://localhost:4200",
+             "https://localhost:4200",
+                    })
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .SetIsOriginAllowed((host) => true));
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+            // Database
+            builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MewaApp")));
+
+            builder.Services.AddIdentity<Model.Model.User, IdentityRole>()
+            .AddEntityFrameworkStores<Context>()
+            .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication()
+            .AddJwtBearer(config =>
+            {
+                config.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                };
+            });
+        }
+    }
+}
--- MewaAppBackend.WebApi/Program.cs
+++ MewaAppBackend.WebApi/Program.cs
@@ -1,46 +1,11 @@
-using MediatR;
-using MewaAppBackend.Model.Interfaces;
-using MewaAppBackend.WebApi;
-using MewaAppBackend.WebApi.UnitOfWork;
+using MewaAppBackend.WebApi.Configuration;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSpaStaticFiles(configuration =>
-{
-    configuration.RootPath = "MewaAppFront/dist";
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy",
-        builder => builder.WithOrigins(new string[]
-        {
-             "https://localhost:7097",
-             "http://localhost:7097",
-             "http://localhost:5097",
-             "https://localhost:5097",
-             "http://localhost:4200",
-             "https://localhost:4200",
-        })
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials()
-        .SetIsOriginAllowed((host) => true));
-});
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MewaApp")));
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MewaApp", Version = "v1" });
-});
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddTransient<IUnitOfWork, GenericUnitOfWork>();
+BasicConfig.Handle(builder);
+ServicesConfig.Handle(builder);
+SwaggerConfig.Handle(builder);
 
 var app = builder.Build();
 
@@ -70,7 +35,7 @@ app.UseSpa(spa =>
     spa.Options.SourcePath = "MewaAppFront";
 
     spa.UseAngularCliServer(npmScript: "start");
-    
+
 });
 
 app.Run();
