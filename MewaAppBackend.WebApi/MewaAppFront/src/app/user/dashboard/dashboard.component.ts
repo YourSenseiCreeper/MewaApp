0d6549636e5fe75cf2ec801f3ac9ebfffc603eea
Błażej Chojaczyk <BlazejChojaczyk@protonmail.com>
2022-04-14 20:23:53 +0200

    Cleaning #2

--- MewaAppBackend.WebApi/MewaAppFront/src/app/user/dashboard/dashboard.component.ts
+++ MewaAppBackend.WebApi/MewaAppFront/src/app/user/dashboard/dashboard.component.ts
@@ -1,9 +1,6 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { MewaAppService } from 'src/app/shared/mewa-app.service';
 import { Link } from 'src/app/shared/models';
-import { NotificationService } from "src/app/shared/notification.service";
 
 @Component({
   selector: 'app-user-dashboard',
@@ -12,16 +9,6 @@ import { NotificationService } from "src/app/shared/notification.service";
   providers: [MewaAppService]
 })
 export class DashboardComponent implements OnInit {
-  userName: string = "Test";
-  folder: string = '';
-  cols : number = 1;
-  gridByBreakpoint = {
-    xl: 5,
-    lg: 3,
-    md: 2,
-    sm: 1,
-    xs: 1
-  };
   links: Link[] = [
     {
       id: 1,
@@ -70,64 +57,10 @@ export class DashboardComponent implements OnInit {
     }
   ];
 
-  constructor(private notificationService: NotificationService,
-    private route: ActivatedRoute,
-    private router: Router,
-    public mewaService: MewaAppService,
-    public breakpointObserver: BreakpointObserver) {
-    breakpointObserver
-    .observe([
-      Breakpoints.XSmall,
-      Breakpoints.Small,
-      Breakpoints.Medium,
-      Breakpoints.Large,
-      Breakpoints.XLarge,
-    ])
-    .subscribe(result => {
-      if (result.matches) {
-        if (result.breakpoints[Breakpoints.XSmall]) {
-          this.cols = this.gridByBreakpoint.xs;
-        }
-        if (result.breakpoints[Breakpoints.Small]) {
-          this.cols = this.gridByBreakpoint.sm;
-        }
-        if (result.breakpoints[Breakpoints.Medium]) {
-          this.cols = this.gridByBreakpoint.md;
-        }
-        if (result.breakpoints[Breakpoints.Large]) {
-          this.cols = this.gridByBreakpoint.lg;
-        }
-        if (result.breakpoints[Breakpoints.XLarge]) {
-          this.cols = this.gridByBreakpoint.xl;
-        }
-      }
-    });
-  }
+  constructor(public mewaService: MewaAppService) { }
+
   ngOnInit(): void {
-    this.route.paramMap.subscribe(paramMap => {
-      this.userName = paramMap.get('userName') ? paramMap.get('userName') as string : 'null';
-      this.folder = paramMap.get('folder') ? paramMap.get('folder') as string : '';
-      if (!!this.folder){
-        // tutaj pobieramy listę linków i folderów dla podanego folderu
-      }
-    });
     // CORS not working :(
     this.mewaService.getAllLinks().subscribe(data => this.links = data);
-  }
-
-  onClickAcction() {
-    this.notificationService.showWarning('Przykładowy warning');
-  }
-
-  onClickAcction1() {
-    this.notificationService.showInfo('Przykładowy info');
-  }
-
-  onClickAcction2() {
-    this.notificationService.showSuccess('Przykładowy success');
-  }
-
-  redirect(url: string) {
-    window.open(url, "_blank");
   }
 } 
