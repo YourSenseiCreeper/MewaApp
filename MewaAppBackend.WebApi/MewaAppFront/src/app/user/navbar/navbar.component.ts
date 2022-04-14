79b4ac5b5fd7d832eacac9d6cbb6b476d007dab9
Błażej Chojaczyk <BlazejChojaczyk@protonmail.com>
2022-04-14 19:11:27 +0200

    Sticky navbar

--- MewaAppBackend.WebApi/MewaAppFront/src/app/user/navbar/navbar.component.ts
+++ MewaAppBackend.WebApi/MewaAppFront/src/app/user/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  navbarFixed: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll') onScroll() {
+    if(window.scrollY > 0) {
+      this.navbarFixed = true;
+    }
+    else {
+      this.navbarFixed = false;
+    }
   }
 
 }
