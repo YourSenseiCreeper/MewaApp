dbd22cae2ce8073914a68701c8ede0d05cd5cdfa
Piotr Wereszczyński <tomanycreeps@gmail.com>
2022-04-28 16:21:59 +0200

    Seeder cleanup

--- MewaAppBackend.Bussines/Seed/MainSeeder.cs
+++ MewaAppBackend.Bussines/Seed/MainSeeder.cs
@@ -16,90 +16,15 @@ namespace MewaAppBackend.Business.Seed
             var userSeeder = new UserSeeder(userManager);
             await userSeeder.Seed();
 
+            // TAGS
+            await TagsSeeder.Seed(context);
+
             var linkSeeder = new LinkSeeder(context);
             await linkSeeder.Seed();
 
-            var adminUser = await SeedUsersAsync(userManager);
-            await context.SaveChangesAsync();
-
-            //await SeedImagesAsync(context);
-            await SeedTagsAsync(context, adminUser);
-            await context.SaveChangesAsync();
-
-            await SeedLinksAsync(context, adminUser);
             //await SeedGroupsAsync(context);
             await context.SaveChangesAsync();
 
-            await TagsSeeder.Seed(context,adminUser);
-        }
-
-        private static async Task<User> SeedUsersAsync(UserManager<User> userManager)
-        {
-            var adminUser = await userManager.FindByNameAsync("admin");
-            if (adminUser == null)
-            {
-                adminUser = new User { UserName = "admin", Email = "admin@mewaapp.pl", EmailConfirmed = true };
-                await userManager.CreateAsync(adminUser, "123qwe");
-
-                if (adminUser == null)
-                {
-                    throw new Exception("The password is probably not strong enough!");
-                }
-            }
-
-            return adminUser;
-        }
-
-        private static async Task SeedLinksAsync(Context context, User user)
-        {
-            var existingLinks = await context.Link.ToListAsync();
-            if (existingLinks.Count == 0)
-            {
-                var tags = await context.Tag.ToListAsync();
-                var youTubeTag = tags.First(t => t.Name == "YouTube");
-                var twitterTag = tags.First(t => t.Name == "Twitter");
-                var politechnikaTag = tags.First(t => t.Name == "Politechnika");
-                var redditTag = tags.First(t => t.Name == "Reddit");
-                var przepisyTag = tags.First(t => t.Name == "Przepisy");
-                var gotujmyTag = tags.First(t => t.Name == "Gotujmy");
-                var StrimsTags = tags.First(t => t.Name == "Strims.world");
-                var allegroTags = tags.First(t => t.Name == "Allegro");
-                var przepisyTags = tags.First(t => t.Name == "Przepisy");
-                var instagramTags = tags.First(t => t.Name == "Instagram");
-                var wykopTags = tags.First(t => t.Name == "Wykop");
-                var googleMapsTags = tags.First(t => t.Name == "GoogleMaps");
-                var tiktokTags = tags.First(t => t.Name == "Tiktok");
-                var twitchTags = tags.First(t => t.Name == "Twitch");
-                var amazonTags = tags.First(t => t.Name == "Amazon");
-                var wyspagierTags = tags.First(t => t.Name == "Wyspagier");
-                var chomikujTags = tags.First(t => t.Name == "Chomikuj");
-
-                var newLinks = new List<Link>
-                {
-                    new Link { Name = "Link do YouTube", Url = "https://youtube.com", OwnerId = user.Id, Tags = new List<Tag> { youTubeTag } },
-                    new Link { Name = "Link do Twittera", Url = "https://twitter.com", OwnerId = user.Id, Tags = new List<Tag> { twitterTag } },
-                    new Link { Name = "Link do materiałów ze studiów", Url = "https://dropbox.com", OwnerId = user.Id, Tags = new List<Tag> { politechnikaTag } }
-                };
-                await context.Link.AddRangeAsync(newLinks);
-            }
-        }
-
-        private static async Task SeedTagsAsync(Context context, User user)
-        {
-            var existingTags = await context.Tag.ToListAsync();
-            if (existingTags.Count == 0)
-            {
-                var newTags = new List<Tag>
-                {
-                    new Tag { Name = "YouTube", Description = "Związane z YouTube", UserId = user.Id },
-                    new Tag { Name = "Twitter", Description = "Związane z Twitterem", UserId = user.Id},
-                    new Tag { Name = "Politechnika", Description = "Związane z politechniką", UserId = user.Id},
-                    new Tag { Name = "Reddit", Description = "Związane z redditem", UserId = user.Id },
-                    new Tag { Name = "Reddit", Description = "Związane z redditem", UserId = user.Id },
-
-                };
-                await context.Tag.AddRangeAsync(newTags);
-            }
         }
 
         private static async Task SeedGroupsAsync(Context context)
--- MewaAppBackend.Bussines/Seed/TagsSeeder.cs
+++ MewaAppBackend.Bussines/Seed/TagsSeeder.cs
@@ -1,5 +1,4 @@
 using MewaAppBackend.Model.Model;
-using Microsoft.AspNetCore.Identity;
 using MewaAppBackend.WebApi;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,53 +10,77 @@ namespace MewaAppBackend.Business.Seed
         public const string Youtube = "Youtube";
         public const string Twitter = "Twitter";
         public const string Politechnika = "Politechnika";
+        public const string Reddit = "Reddit";
+        public const string Gotujemy = "Gotujemy";
+        public const string Allegro = "Allegro";
+        public const string Strims = "Strims";
+        public const string Instagram = "Instagram";
+        public const string Wykop = "Wykop";
+        public const string GoogleMaps = "GoogleMaps";
+        public const string Tiktok = "Tiktok";
         public const string Przepisy = "Przepisy";
+        public const string Twitch = "Twitch";
+        public const string Amazon = "Amazon";
+        public const string Wyspagier = "Wyspagier";
+        public const string Chomikuj = "Chomikuj";
+        public const string Spotify = "Spotify";
+        public const string Soundcloud = "Soundcloud";
+        public const string Plantpur = "Plantpur";
+        public const string Withgoogle = "Withgoogle";
+        public const string Booking = "Booking";
+        public const string LinkedIn = "LinkedIn";
+        public const string Pinterest = "Pinterest";
+        public const string Intercity = "Intercity";
+        public const string MediaExpert = "MediaExpert";
+        public const string MediaMarkt = "MediaMarkt";
+        public const string Libgen = "Libgen";
+        public const string Zooplus = "Zooplus";
+        public const string Fabrykasily = "Fabrykasily";
+        public const string Polki = "Polki";
+        public const string Gazeta = "Gazeta";
     }
 
     public class TagsSeeder
     {
-        public static async Task Seed(Context context, User user)
+        public static async Task Seed(Context context)
         {
+            var user = context.Users.First(u => u.UserName == UserSeederNames.User1);
             var existingTags = await context.Tag.ToListAsync();
             if (existingTags.Count == 0)
             {
                 var newTags = new List<Tag>
                 {
-                    new Tag { Name = "Youtube" , Description = "Związane z YouTube", UserId = user.Id },
-                    new Tag { Name = "Twitter", Description = "Związane z Twitterem", UserId = user.Id},
-                    new Tag { Name = "Politechnika", Description = "Związane z politechniką", UserId = user.Id},
-                    new Tag { Name = "Reddit", Description = "Związane z redditem", UserId = user.Id },
-                    new Tag { Name = "Przepisy", Description = "Związane z przepisami", UserId = user.Id },
-                    new Tag { Name = "Gotujmy", Description = "Związane z gotowaniem", UserId = user.Id },
-                    new Tag { Name = "Allegro", Description = "Związane z zakupami", UserId = user.Id },
-                    new Tag { Name = "Strims", Description = "Związane z oglądaniem meczy", UserId = user.Id },
-                    new Tag { Name = "Instagram", Description = "Związane z instagramem", UserId = user.Id },
-                    new Tag { Name = "Wykop", Description = "Związane z wykopem", UserId = user.Id },
-                    new Tag { Name = "GoogleMaps", Description = "Związane z lokalizacją", UserId = user.Id },
-                    new Tag { Name = "Tiktok", Description = "Związane z tik tokiem", UserId = user.Id },
-                    new Tag { Name = "Przepisy", Description = "Związane z przepisami", UserId = user.Id },
-                    new Tag { Name = "Twitch", Description = "Związane z Twitch'em", UserId = user.Id },
-                    new Tag { Name = "Amazon", Description = "Związane z Amazon'em", UserId = user.Id },
-                    new Tag { Name = "Wyspagier", Description = "Związane z Wyspagier", UserId = user.Id },
-                    new Tag { Name = "Chomikuj", Description = "Związane z Chomikuj", UserId = user.Id },
-                    new Tag { Name = "Przepisy", Description = "Związane z przepisami", UserId = user.Id },
-                    new Tag { Name = "Spotify", Description = "Związane z Spotify", UserId = user.Id },
-                    new Tag { Name = "Soundcloud", Description = "Związane z Soundcloud", UserId = user.Id },
-                    new Tag { Name = "Plantpur", Description = "Związane z kalkulator snu", UserId = user.Id },
-                    new Tag { Name = "Soundcloud", Description = "Związane z Soundcloud", UserId = user.Id },
-                    new Tag { Name = "Withgoogle", Description = "Związane z kursami z Google'a", UserId = user.Id },
-                    new Tag { Name = "Soundcloud", Description = "Związane z Soundcloud", UserId = user.Id },
-                    new Tag { Name = "Linkedin", Description = "Związane z Linkedin'em", UserId = user.Id },
-                    new Tag { Name = "Booking", Description = "Związane z Bookingiem", UserId = user.Id },
-                    new Tag { Name = "Pinterest", Description = "Związane z Pinterestem", UserId = user.Id },
-                    new Tag { Name = "Intercity", Description = "Związane z Intercity", UserId = user.Id },
-                    new Tag { Name = "MediaExpert", Description = "Związane z MediaExpert", UserId = user.Id },
-                    new Tag { Name = "MediaMarkt", Description = "Związane z MediaMarkt", UserId = user.Id },
-                    new Tag { Name = "Libgen", Description = "Związane z e-book'ami", UserId = user.Id },
-                    new Tag { Name = "Zooplus", Description = "Związane z rzeczami dla zwierząt", UserId = user.Id },
-                    new Tag { Name = "Fabrykasily", Description = "Związane z treningiem", UserId = user.Id },
-                    new Tag { Name = "Polki", Description = "Związane z plotkami", UserId = user.Id },
-                    new Tag { Name = "Gazeta", Description = "Związane z gazetą", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Youtube , Description = "Związane z YouTube", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Twitter, Description = "Związane z Twitterem", UserId = user.Id},
+                    new Tag { Name = TagsSeederNames.Politechnika, Description = "Związane z politechniką", UserId = user.Id},
+                    new Tag { Name = TagsSeederNames.Reddit, Description = "Związane z redditem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Przepisy, Description = "Związane z przepisami", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Gotujemy, Description = "Związane z gotowaniem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Allegro, Description = "Związane z zakupami", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Strims, Description = "Związane z oglądaniem meczy", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Instagram, Description = "Związane z instagramem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Wykop, Description = "Związane z wykopem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.GoogleMaps, Description = "Związane z lokalizacją", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Tiktok, Description = "Związane z tik tokiem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Twitch, Description = "Związane z Twitch'em", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Amazon, Description = "Związane z Amazon'em", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Wyspagier, Description = "Związane z Wyspagier", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Chomikuj, Description = "Związane z Chomikuj", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Spotify, Description = "Związane z Spotify", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Soundcloud, Description = "Związane z Soundcloud", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Plantpur, Description = "Związane z kalkulator snu", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Withgoogle, Description = "Związane z kursami z Google'a", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.LinkedIn, Description = "Związane z Linkedin'em", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Booking, Description = "Związane z Bookingiem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Pinterest, Description = "Związane z Pinterestem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Intercity, Description = "Związane z Intercity", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.MediaExpert, Description = "Związane z MediaExpert", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.MediaMarkt, Description = "Związane z MediaMarkt", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Libgen, Description = "Związane z e-book'ami", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Zooplus, Description = "Związane z rzeczami dla zwierząt", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Fabrykasily, Description = "Związane z treningiem", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Polki, Description = "Związane z plotkami", UserId = user.Id },
+                    new Tag { Name = TagsSeederNames.Gazeta, Description = "Związane z gazetą", UserId = user.Id },
 
                 };
                 await context.Tag.AddRangeAsync(newTags);
