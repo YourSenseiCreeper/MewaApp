e4cab88ecefe3b7907c8d46aaaf8898eb5c7125c
Wojciech Buńka <44346481+TheWerou@users.noreply.github.com>
2022-10-09 17:21:43 +0200

    Fixing register

--- MewaAppBackend.WebApi/MewaAppFront/src/app/auth/register/register.component.ts
+++ MewaAppBackend.WebApi/MewaAppFront/src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from '@angular/router';
+import { catchError, EMPTY } from 'rxjs';
 import { RegisterCommand } from 'src/app/shared/models';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { EmailService } from 'src/app/shared/services/email.service';
@@ -56,18 +57,17 @@ export class RegisterComponent {
       email: rawForm.email,
       password: rawForm.password
     } as RegisterCommand
-    this.service.register(command).subscribe(r => {
-      if (r.success) {
+    this.service.register(command)
+      .pipe(
+        catchError((err, caught) => {
+          this.notification.showError("Coś poszło nie tak");
+          this.submitDisabled = false;
+        return EMPTY;
+      }))
+      .subscribe(token => {
         this.notification.showSuccess("Pomyślnie zarejestrowano");
         this.router.navigate(['../']);
-      } else {
-        this.notification.showError("Coś poszło nie tak");
-        this.submitDisabled = false;
-      }
-    }, error => {
-      this.notification.showError("Problem z połączeniem: " + error.message);
-      this.submitDisabled = false;
-    });
+      });
   }
 
   getErrorMessage(control: string): string {
